4f60709d11e3f20936ddbc8cbb278318da0dcc16
Nuniemsis <nunocarvalhodossantos@gmail.com>
2019-09-22 19:45:46 +0200

    update app.py

--- app.py
+++ app.py
@@ -1,30 +1,20 @@
 from flask import Flask
 from flask_cors import CORS
 from flask import Response
+from flask import request
 
 from geolite2 import geolite2
 app = Flask(__name__)
-from flask import request
 
-
-app = Flask(__name__)
 CORS(app)
 @app.route('/')
-def ip_geolocator():
+def ip_geolocate():
     ip = request.args.get('ip')
     reader = geolite2.reader()
     try:
-
         match = reader.get(ip)
     except ValueError as e:
         print(e)
-    print('My IP info:', match["country"])
-    return match
-
-@app.route('/bad')
-def bad():
-    reader = geolite2.reader()
-    match = reader.get('1-23123')
     print('My IP info:', match["country"])
     return match
 
